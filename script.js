3f08fb7c51b19e9ebc678d03ea5ba3052a3d24ea
sanketsj07 <sanketjadhav7595@gmail.com>
2024-05-05 15:28:13 -0700

    html and js file added...

---
+++ script.js
@@ -0,0 +1,14 @@
+fnction capitalizeFLetter() {
+	let string = 'sanket';
+	console.log(string.replace(/^./, string[0].toUpperCase()));
+}
+	
+	capitalizeFLetter()
+	
+	function capitalizeFLetter(){
+		let string="Sanket";
+		
+		console.log(string.replace(/^./, string.toUpperCase()));
+	}
+	
+	capitalizeFLetter()
